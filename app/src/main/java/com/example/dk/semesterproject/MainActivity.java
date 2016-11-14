72b0772f1ec78a5eb04cfb1f478b73b7fbd53370
github <eotoude@gmail.com>
2016-11-14 16:40:56 -0500

    Moved register to the header area to maintain consistency on all pages. Added footer to the register page. Created RegisterActivity.

--- app/src/main/java/com/example/dk/semesterproject/MainActivity.java
+++ app/src/main/java/com/example/dk/semesterproject/MainActivity.java
@@ -1,17 +1,34 @@
 package com.example.dk.semesterproject;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends Activity {
+    private Button mLogin;
+    private Button mRegister;
+    private Button mForgotPassword;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        // there we go
+        // Get the buttons references
+        mLogin= (Button) findViewById(R.id.login);
+        mRegister= (Button) findViewById(R.id.registerHelp);
+        mForgotPassword= (Button) findViewById(R.id.forgotPasswordHelp);
 
-        //dkdkdkdkdk
+        // Attach listeners to the buttons
+        mRegister.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Start the RegisterActivity
+                Intent intent= new Intent(getApplicationContext(), RegisterActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
---
+++ app/src/main/java/com/example/dk/semesterproject/RegisterActivity.java
@@ -0,0 +1,17 @@
+package com.example.dk.semesterproject;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Register Page. Provides means to the user to register
+ */
+
+public class RegisterActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.register_page);
+    }
+}
