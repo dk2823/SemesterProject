da4a4f996b392e3b09134c2f3f8c8494ea36f181
github <eotoude@gmail.com>
2016-11-15 09:41:20 -0500

    11/15/2016 9:41am

---
+++ app/src/main/java/com/example/dk/semesterproject/PasswordRecoveryActivity.java
@@ -0,0 +1,18 @@
+package com.example.dk.semesterproject;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.widget.Button;
+
+/**
+ * Activity that allows the user to recover his/her password
+ */
+
+public class PasswordRecoveryActivity extends Activity {
+    private Button mSendPasswordButton
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
