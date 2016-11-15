b657d1d4b69851ba025d6d1174c2a586250309dd
github <eotoude@gmail.com>
2016-11-14 21:05:21 -0500

    11/14/2016 9:05pm

--- app/src/main/java/com/example/dk/semesterproject/MainActivity.java
+++ app/src/main/java/com/example/dk/semesterproject/MainActivity.java
@@ -5,11 +5,16 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends Activity {
+    private static final int CREATE_ACCOUNT= 0;
+
     private Button mLogin;
     private Button mRegister;
     private Button mForgotPassword;
+    private TextView mStatus;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,14 +26,28 @@ public class MainActivity extends Activity {
         mRegister= (Button) findViewById(R.id.registerHelp);
         mForgotPassword= (Button) findViewById(R.id.forgotPasswordHelp);
 
-        // Attach listeners to the buttons
+        // Get the status textView
+        mStatus= (TextView) findViewById(R.id.registrationOrpasswordRecoveryResult);
+
+        // Get the reference to the textview to display whether the account creation was a success
+        // or not, or whether the password recovery email has been sent
+
+        // Attach listeners to the buttons. When the user clicks on the registration button
+        // then the RegisterActivity will start
         mRegister.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 // Start the RegisterActivity
                 Intent intent= new Intent(getApplicationContext(), RegisterActivity.class);
-                startActivity(intent);
+                startActivityForResult(intent, CREATE_ACCOUNT);
             }
         });
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (requestCode==CREATE_ACCOUNT && resultCode==RESULT_OK) {
+            mStatus.setText(R.string.create_success);
+        }
     }
 }
--- app/src/main/java/com/example/dk/semesterproject/RegisterActivity.java
+++ app/src/main/java/com/example/dk/semesterproject/RegisterActivity.java
@@ -2,16 +2,79 @@ package com.example.dk.semesterproject;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 /**
  * Register Page. Provides means to the user to register
  */
 
 public class RegisterActivity extends Activity {
+    private static final int SUCCESS= 0;
+    private static final int USERNAME_IN_USE= 1;
+    private static final int PASSWORDS_DO_NOT_MATCH= 2;
+    private static final int ENTRY_MISSING= 3;
+
+    private Button mRegister;
+    private EditText mUsername;
+    private EditText mPassword;
+    private EditText mConfirm;
+    private TextView mRegistrationStatus;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.register_page);
+
+        // Get the textViews and the button reference
+        mRegister= (Button) findViewById(R.id.registerButton);
+        mUsername= (EditText) findViewById(R.id.username);
+        mPassword= (EditText) findViewById(R.id.password);
+        mConfirm= (EditText) findViewById(R.id.confirm_password);
+        mRegistrationStatus= (TextView) findViewById(R.id.registrationStatus);
+
+        // When the Register button is pressed, attempt to register this user.
+        // If it is successful send the user back to the MainActivity
+        mRegister.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                switch (check()) {
+                    case SUCCESS:
+                        setResult(RESULT_OK);
+                        finish();
+                        break;
+                    case USERNAME_IN_USE:
+                        mRegistrationStatus.setText(R.string.username_in_use);
+                        break;
+                    case PASSWORDS_DO_NOT_MATCH:
+                        mRegistrationStatus.setText(R.string.password_not_matching_confirm);
+                        break;
+                    default:
+                        mRegistrationStatus.setText(R.string.entry_missing);
+                }
+            }
+        });
+    }
+
+    /**
+     * Returns SUCCESS if username is created otherwise returns the appropriate code
+     */
+    private int check() {
+        String username= mUsername.getText().toString().trim();
+        String password= mPassword.getText().toString();
+        String confirmPassword= mConfirm.getText().toString();
+
+        if (!username.equals("") && !password.equals("") && !confirmPassword.equals("")) {
+            // TODO Check whether the username is in use
+
+            // Check whether the password matches the confirmation password
+            if (password.equals(confirmPassword))
+                return SUCCESS;
+            else
+                return PASSWORDS_DO_NOT_MATCH;
+        }
+        return ENTRY_MISSING;
     }
 }
