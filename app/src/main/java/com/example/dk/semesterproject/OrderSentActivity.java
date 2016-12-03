fba2b3e39c0d0f182fd950cf5ca919980dd65049
Alexander Padilla <padilla288@gmail.com>
2016-12-03 11:55:41 -0500

    order sent updates more

--- app/src/main/java/com/example/dk/semesterproject/OrderSentActivity.java
+++ app/src/main/java/com/example/dk/semesterproject/OrderSentActivity.java
@@ -8,17 +8,25 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
+import android.widget.ProgressBar;
+import android.widget.SeekBar;
 
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Random;
+import java.util.Timer;
+import java.util.TimerTask;
+
 import models.Ingredient;
 import models.Restaurant;
 
 public class OrderSentActivity extends Activity {
+    private SeekBar mSeekBar;
+    private Timer timer;
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.order_sent_page);
+        mSeekBar = (SeekBar) findViewById(R.id.seekBar4);
         //TODO  access database ingredient time, and restaurant NAME and Address
         Intent intent = getIntent();
         List<Ingredient> ing = (ArrayList<Ingredient>) intent.getSerializableExtra("ingredient");
@@ -35,8 +43,6 @@ public class OrderSentActivity extends Activity {
         int i1 = r.nextInt(99);
         String s1 = "Order # " + i1 ;
 
-
-
         ArrayList<String> s = new ArrayList<String>();
         s.add(s1);
         s.add(rest.getName());
@@ -46,5 +52,26 @@ public class OrderSentActivity extends Activity {
                 R.layout.activity_order_sent_listview, s);
         ListView listView = (ListView) findViewById(R.id.sampleListView);
         listView.setAdapter(adapter);
+        startSeek(time);
+
+        Intent i = new Intent(this, OrderPickupActivity.class);
+        i.putExtra("orderNum", s1);
+        i.putExtra("address", rest.getAddress());
+        i.putExtra("name", rest.getName());
+        startActivity(i);
+    }
+
+    private void startSeek(int t) {
+        mSeekBar.setMax(t*1000);
+        mSeekBar.setProgress(0);
+        timer = new Timer();
+        timer.schedule(new TimerTask() {
+            @Override
+            public void run() {
+                int p = mSeekBar.getProgress();
+                p += 1000;
+                mSeekBar.setProgress(p);
+            }
+        }, 1, 1000);
     }
 }
