08a5898d9074f0c6999036127805ac28fbab0300
Alexander Padilla <padilla288@gmail.com>
2016-12-03 14:57:52 -0500

    order sent updates more

--- app/src/main/java/com/example/dk/semesterproject/OrderSentActivity.java
+++ app/src/main/java/com/example/dk/semesterproject/OrderSentActivity.java
@@ -27,14 +27,16 @@ import models.Restaurant;
 
 public class OrderSentActivity extends Activity {
     private SeekBar mSeekBar;
-    private Timer timer;
+
+    private Intent i;
+
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.order_sent_page);
         mSeekBar = (SeekBar) findViewById(R.id.seekBar4);
         //  access database ingredient time, and restaurant NAME and Address
-        Intent intent = getIntent();
-        final String ingredientIds = intent.getExtras().getString(OrderActivity.INGREDIENTS);
+        i = getIntent();
+        final String ingredientIds = i.getExtras().getString(OrderActivity.INGREDIENTS);
         IngredientDBO ingredientDBO = new IngredientDBO(OrderSentActivity.this);
         RestaurantDBO restaurantDBO = new RestaurantDBO(OrderSentActivity.this);
         String[] ids = getIds(new String(ingredientIds));
@@ -60,12 +62,12 @@ public class OrderSentActivity extends Activity {
             time += ing.get(i).getCookTime();
         }
         Log.i("time tag", "" + time);
-        Random r = new Random();
-        int i1 = r.nextInt(99);
-        String s1 = "Order # " + i1 ;
+//        Random r = new Random();
+//        int i1 = r.nextInt(99);
+//        String s1 = "Order # " + i1 ;
 
         ArrayList<String> s = new ArrayList<String>();
-        s.add(s1);
+       // s.add(s1);
         s.add(rest.getName());
         s.add(rest.getAddress());
         s.add("Total Time: " + time);
@@ -73,36 +75,63 @@ public class OrderSentActivity extends Activity {
                 R.layout.activity_order_sent_listview, s);
         ListView listView = (ListView) findViewById(R.id.sampleListView);
         listView.setAdapter(adapter);
-        startSeek(time);
+        //Long t = time*1000l;
+        new OrderSentBackground().execute(time*1000);
+        //startSeek(time);
 
-        Intent i = new Intent(this, OrderPickupActivity.class);
-        i.putExtra("orderNum", s1);
-        i.putExtra(DBHelper.COLUMN_RESTAURANT_ADDRESS, rest.getAddress());
-        i.putExtra(DBHelper.COLUMN_RESTAURANT_NAME, rest.getName());
-        startActivity(i);
+//        Intent i = new Intent(this, OrderPickupActivity.class);
+//        i.putExtra("orderNum", s1);
+//        i.putExtra(DBHelper.COLUMN_RESTAURANT_ADDRESS, rest.getAddress());
+//        i.putExtra(DBHelper.COLUMN_RESTAURANT_NAME, rest.getName());
+//        startActivity(i);
     }
 
-    private void startSeek(int t) {
-        mSeekBar.setMax(t);
-        mSeekBar.setProgress(0);
-        timer = new Timer();
-        timer.schedule(new TimerTask() {
-            @Override
-            public void run() {
-                int p = mSeekBar.getProgress();
-                p += 1000;
-                mSeekBar.setProgress(p);
-            }
-        }, 1, 1000);
-    }
+//    private void startSeek(Long t) {
+//        mSeekBar.setMax(t);
+//        mSeekBar.setProgress(0);
+//        timer = new Timer();
+//        timer.schedule(new TimerTask() {
+//            @Override
+//            public void run() {
+//                int p = mSeekBar.getProgress();
+//                p += 1000;
+//                mSeekBar.setProgress(p);
+//            }
+//        }, 1, 1000);
+//    }
 
     private String[] getIds(String ids){
         String[] result = ids.split(",");
 
         return result;
     }
-//    private class OrderSentBackground extends AsyncTask<Long, Long, Void> {
-//
-//
-//    }
+    private class OrderSentBackground extends AsyncTask<Integer, Integer, Void> {
+
+        protected Void doInBackground(Integer... params) {
+            for (int i = 0; i < params[0]; i+=1000) {
+                try {
+                    Thread.sleep(1000);
+                    publishProgress(i);
+
+
+                } catch (InterruptedException e) {
+                    Thread.interrupted();
+                }
+            }
+            return null;
+        }
+        protected void onPreExecute(){
+            super.onPreExecute();
+        }
+
+        protected void onProgressUpdate(Integer... values){
+           // super.onProgressUpdate(values[0]);
+            mSeekBar.setProgress(values[0]);
+        }
+
+        protected void onPostExecute(Void result) {
+            i.setClass(OrderSentActivity.this, OrderPickupActivity.class);
+            startActivity(i);
+        }
+    }
 }
